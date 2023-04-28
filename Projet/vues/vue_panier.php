5a76d1ff9ecd002020627faca9158574c730d352
hichem-hammami <61579087+hichem-hammami@users.noreply.github.com>
2023-04-28 13:09:24 +0100

    final

--- Projet/vues/vue_panier.php
+++ Projet/vues/vue_panier.php
@@ -1,6 +1,6 @@
 <?php $titre="Panier d'achat"?>
 <?php ob_start();?>
-<html>
+   <html>
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
